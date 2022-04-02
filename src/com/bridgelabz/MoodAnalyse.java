5aa48ec9107571932a6b3cc6d83ba67f95158074
Chougulenik <nikitachougule47@gmail.com>
2022-04-02 12:39:08 -0400

    added

--- src/com/bridgelabz/MoodAnalyse.java
+++ src/com/bridgelabz/MoodAnalyse.java
@@ -3,21 +3,29 @@ package com.bridgelabz;
 public class MoodAnalyse {
 	
 	private String message;
-
+	
+	public MoodAnalyse() {
+		
+	}
+	
 	public MoodAnalyse(String message) {
 		this.message = message;
 	}
 	
 	public String returnEmotion() {
-		if(message == "I am in sad mood") {
-			return "Sad";
-		}
-		else if(message == "I am in any mood") {
-			return "happy";
-		}
-		return "Happy";
-	}
-	 
-	
-		
+		try {
+			if(message.equals("I am in sad mood")) {
+				return "Sad";
+			}
+			else if(message.equals("I am in any mood")) {
+				return "happy";
+			}
+			else {
+				throw new NullPointerException();
+			}
+		} catch (Exception e) {
+			System.out.println("Null message found");
+			return "Happy";
+		}		
+	}		
 }
